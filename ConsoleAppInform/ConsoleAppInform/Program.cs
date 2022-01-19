f719475100cc9e3925d9064296102ca9d2b33e27
Nicholas <93314425+koyjiin@users.noreply.github.com>
2022-01-19 19:02:07 +0300

    Add files via upload

---
+++ ConsoleAppInform/ConsoleAppInform/Program.cs
@@ -0,0 +1,14 @@
+﻿//task 2
+
+Console.Write("Введите число x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+
+double sqrt = Math.Sqrt(Math.Abs(2 * Math.Pow(x, 2) - Math.Abs(Math.Sin(x)) * Math.Tan(x)));
+double pow_sqrt = Math.Pow(sqrt, 5);
+double a = pow_sqrt * Math.Pow(2.5, Math.Cos(x));
+
+double b = 0.625 + 2 * Math.Log2(x + 7.5);
+
+double y = a / b;
+
+Console.WriteLine(y);
---
+++ ConsoleAppInform/ConsoleAppInformTask1/Program.cs
@@ -0,0 +1,10 @@
+﻿//task 1
+
+Console.WriteLine("Введите радиус: ");
+double r = Convert.ToDouble(Console.ReadLine());
+
+double S = Math.PI * Math.Pow(r, 2);
+double P = 2 * r * Math.PI;
+
+Console.WriteLine("Площадь = " + S + "\n");
+Console.WriteLine("Периметр = " + P);
