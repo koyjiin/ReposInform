1f91ee2f0d3e348a01b6bf0f1677bf626fee8a15
Nicholas <93314425+koyjiin@users.noreply.github.com>
2022-01-19 19:05:14 +0300

    Add files via upload

---
+++ ConsoleAppInform/ConsoleAppInformTask7/Program.cs
@@ -0,0 +1,20 @@
+﻿//task 7
+
+Console.WriteLine("Введите число строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] mas = new int[m, n];
+Random rnd = new Random();
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        mas[i, j] = rnd.Next(0, 10);
+        Console.Write(mas[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+
